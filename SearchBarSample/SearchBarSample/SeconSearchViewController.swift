534bc7171127f8ef95a7dcae02091d0401409ae0
Israel Manzo <israman814@gmail.com>
2017-05-20 16:32:25 -0400

    Updating search bar

--- SearchBarSample/SearchBarSample/SeconSearchViewController.swift
+++ SearchBarSample/SearchBarSample/SeconSearchViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 
-class SeconSearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class SeconSearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    var tableData = ["Ecuador", "USA", "Mexico", "Canada", "Colombia", "Venezuela", "Peru", "Brasil", "Chile", "Bolivia", "Argentina", "Uruguay", "Paraguay", "Guatemala", "El Salvador", "Honduras", "Nicargua", "Panama", "Costa Rica", "Cuba", "Rep Dominicana", "Granada", "Haiti", "Bahamas", "Trinidad Tobago"]
+    
+    var isSearching = false
+    
+    var filterData = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +26,10 @@ class SeconSearchViewController: UIViewController, UITableViewDelegate, UITableV
         tableView.delegate = self
         tableView.dataSource = self
         
-        
+        searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.done
+        searchBar.barTintColor = UIColor.black
+
     }
 
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -29,12 +37,36 @@ class SeconSearchViewController: UIViewController, UITableViewDelegate, UITableV
     }
   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        
+        if isSearching {
+            return filterData.count
+        }
+        return tableData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
         
-        return cell!
+        if isSearching {
+            cell?.textLabel?.text = filterData[indexPath.row]
+        } else {
+        
+            cell?.textLabel?.text = tableData[indexPath.row]
+        }
+            return cell!
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == nil || searchBar.text == "" {
+            isSearching = false
+            view.endEditing(true)
+            tableView.reloadData()
+        } else {
+            isSearching = true
+            filterData = tableData.filter({ (text) -> Bool in
+                return text.lowercased().range(of: searchText.lowercased()) != nil
+            })
+            tableView.reloadData()
+        }
     }
 }
